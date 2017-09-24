3c26ae7734cfb74b29edbd167d744cb7427add1e
rabinzon <ildarrazin@gmail.com>
2017-09-24 20:55:46 +0300

    refactor(prime): optimize isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,16 +1,23 @@
 import game from '..';
 import getRandomInt from '../utils';
 
-const isPrime = (number, i = 2) => {
-  if (number === i) return true;
-  if (number % i === 0) return false;
-  return isPrime(number, i + 1);
+const isPrime = (number) => {
+  const firstDivider = 2;
+  const lastDivider = Math.ceil(Math.sqrt(number));
+
+  const checkPrimality = (divider) => {
+    if (divider === number || lastDivider < divider) return true;
+    if (number % divider === 0) return false;
+    return checkPrimality(divider + 1);
+  };
+
+  return checkPrimality(firstDivider);
 };
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
 const getQuestionAndAnswer = () => {
-  const question = getRandomInt(1, 199);
+  const question = getRandomInt(2, 199);
   const answer = isPrime(question) ? 'yes' : 'no';
 
   return {
